1e28f9f3cb2f4e16692d8188abf2b0c310913ff8
David Nuon <davidnuongm@gmail.com>
2018-06-04 20:56:31 -0700

    Added advancing with keyboard

--- App.elm
+++ App.elm
@@ -1,14 +1,21 @@
 module App exposing (..)
 
-import Html exposing (Html, button, div, text, program)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, text, program, Attribute)
+import Html.Attributes exposing (attribute)
+import Html.Events exposing (on, onClick, keyCode, onInput)
 import Array
+import Json.Decode as Json
+import Keyboard
 
 type alias Slides msg =
   { 
     children : List (Html.Html msg),
     classes: List (String)
   }
+
+onKeyDown : (Int -> msg) -> Attribute msg
+onKeyDown tagger =
+  on "keydown" (Json.map tagger keyCode)
 
 slides : List (Slides msg)
 slides = [
@@ -38,7 +45,10 @@ init =
     ( 0, Cmd.none )
 
 -- MESSAGES
-type Msg = Increment | Decrement
+type Msg 
+    = Increment
+    | Decrement
+    | KeyMsg Keyboard.KeyCode
 
 -- VIEW
 view : Model -> Html Msg
@@ -62,18 +72,30 @@ view model =
 -- UPDATE
 update : Msg -> Model -> (Model, Cmd msg)
 update msg model =
-    case msg of
-        Increment -> 
-            ((model + 1) % List.length slides, Cmd.none)
-        Decrement -> 
-            (max 0 (model - 1), Cmd.none)
+    let
+        nextSlide = ((model + 1) % List.length slides, Cmd.none)
+        prevSlide = (max 0 (model - 1), Cmd.none)
+    in
+        case msg of
+            KeyMsg key -> 
+                if key == 39 then -- right arrow
+                    nextSlide
 
--- SUBSCRIPTIONS
+                else if key == 37 then -- left arrow
+                    prevSlide
+
+                else
+                    (model, Cmd.none)
+            Increment -> 
+                nextSlide
+            Decrement -> 
+                prevSlide
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ Keyboard.downs KeyMsg ]
 
--- MAIN
 
 main : Program Never Model Msg
 main =
